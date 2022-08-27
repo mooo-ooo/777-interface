613b250d6068778d1856b679aec7f9440b8e0eb2
huquduy <huquduy@gmail.com>
2022-08-28 00:31:44 +0700

    Feat: Web3Provider

--- pages/Providers.tsx
+++ pages/Providers.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Web3ReactProvider } from '@web3-react/core'
+import { getLibrary } from 'utils/web3React'
 import ThemeProvider from 'contexts/ThemeContext'
 import { LanguageProvider } from 'contexts/Localization/Provider'
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <ThemeProvider>
-      <LanguageProvider>
-        {children}
-      </LanguageProvider>
-    </ThemeProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <ThemeProvider>
+        <LanguageProvider>
+          {children}
+        </LanguageProvider>
+      </ThemeProvider>
+    </Web3ReactProvider>
+    
   )
 }
 
